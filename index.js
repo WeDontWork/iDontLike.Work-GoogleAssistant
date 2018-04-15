7d1d85bdef6d88c4a2179c27deed78da0412a216
Jibin Mathews <jibinmathews7@gmail.com>
2018-04-15 14:06:09 +0530

    initial release

--- index.js
+++ index.js
@@ -2,9 +2,29 @@
 
 const serverless = require('serverless-http');
 const express = require('express');
-
+const request = require('request');
 const app = express();
 
-app.get('/get-reason', function(req, res) {});
+const REASON_URL = `https://s3.ap-south-1.amazonaws.com/idontlikework/wfh-reasons.json`;
+
+app.post('/get-reason', function(req, res) {
+  request(REASON_URL, (err, response, body) => {
+    const reasons = JSON.parse(body);
+
+    const reasonCount = reasons.length;
+    const randomReasonIndex = Math.floor(Math.random() * reasonCount);
+
+    const reason = reasons[randomReasonIndex].text;
+
+    res.set('Content-Type', 'application/json');
+
+    res.send(
+      JSON.stringify({
+        speech: reason,
+        displayText: reason,
+      }),
+    );
+  });
+});
 
 module.exports.handler = serverless(app);
